8cc8dc57ad3ed10c1c1b840f7a019601b7962637
sagarpakhrin <sasgarlama826@gmail.com>
2023-01-20 21:57:38 +0545

    Initial commit

---
+++ lib/space-finder-backend-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class SpaceFinderBackendStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'SpaceFinderBackendQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
